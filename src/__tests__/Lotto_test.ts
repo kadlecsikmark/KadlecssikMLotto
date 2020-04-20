3f0c72b1c267d3b1a7e5e5868056f168175a2961
kadlecsikmark <kaddlecsikmark@gmail.com>
2020-04-20 12:42:33 +0200

    Commit

--- src/__tests__/Lotto_test.ts
+++ src/__tests__/Lotto_test.ts
@@ -9,17 +9,25 @@ describe("Lotto osztaly unik test", () => {
         expect(instance2).toBeInstanceOf(Lotto);
     });
 
-    it("Első szám ellenőrzése", async () => {
+    it("Első nyerőszámellenőrzése", async () => {
         expect(instance1.szam1).toBe(5);
         expect(instance2.szam1).toBe(33);
     });
 
-    it("Első szám ellenőrzése", async () => {
+    it("Második nyerőszám ellenőrzése", async () => {
         expect(instance1.szam2).toBe(17);
         expect(instance2.szam2).toBe(45);
     });
+    it("Harmadik nyerőszám ellenőrzése", async () => {
+        expect(instance1.szam3).toBe(22);
+        expect(instance2.szam3).toBe(60);
+    });
+    it("Negyedik nyerőszám ellenőrzése", async () => {
+        expect(instance1.szam4).toBe(69);
+        expect(instance2.szam4).toBe(86);
+    });
 
-    it("Első szám ellenőrzése", async () => {
+    it("Ötödik nyerőszám ellenőrzése", async () => {
         expect(instance1.szam5).toBe(90);
         expect(instance2.szam5).toBe(88);
     });
